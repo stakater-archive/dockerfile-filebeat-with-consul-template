9ef5a265fc81488dc9fda04594d881a8b618b6a7
hazim1093 <hazim_malik@hotmail.com>
2017-02-24 17:10:04 +0500

    Add resources for filebeat v5.2

---
+++ 5.2/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# We just start the filebeat service and then leave consul-template running on the foreground.
+# Here we use the CONSUL_URL environment variable that we defined before. Consul template
+# expects to find a filebeat.ctmpl file in /templates. This is the template that we would
+# mount as a volume from our host. The result is then placed in /etc/filebeat/filebeat.yml
+# where filebeat will be able to read from.
+# We start filebeat in background with the configuration specified once consul-template has rendered the template
+# Filebeat logs will be recorded in /filebeat.log file.
+/etc/init.d/filebeat start &
+consul-template -consul=$CONSUL_URL -template="/templates/filebeat.ctmpl:/etc/filebeat/filebeat.yml:/etc/init.d/filebeat restart > /filebeat.log &"
