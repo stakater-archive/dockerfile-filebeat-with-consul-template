a703fb8fa55b6acd81514ea0a151891660b7b166
hazim1093 <hazim_malik@hotmail.com>
2017-02-27 17:56:49 +0500

    Add script to kill processes

--- 5.2/start.sh
+++ 5.2/start.sh
@@ -6,6 +6,9 @@
 # mount as a volume from our host. The result is then placed in /etc/filebeat/filebeat.yml
 # where filebeat will be able to read from.
 # We start filebeat in background with the configuration specified once consul-template has rendered the template
-# Filebeat logs will be recorded in /filebeat.log file.
-filebeat start &
-consul-template -consul-addr=$CONSUL_URL -template="/templates/filebeat.ctmpl:/etc/filebeat/filebeat.yml:filebeat -c /etc/filebeat/filebeat.yml restart > /filebeat.log &"
+filebeat -c /etc/filebeat/filebeat.yml start &
+
+# Render template and kill all processes related to filebeat via stop-processes.sh
+# runit will start them up again with the new configuration
+consul-template -consul-addr=$CONSUL_URL \
+                -template="/templates/filebeat.ctmpl:/etc/filebeat/filebeat.yml:/etc/scripts/stop-processes.sh filebeat"
